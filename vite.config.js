11d3759838056a31b50a3700824655b4dffbdf16
zoe <zoe_yang@xred.com.tw>
2025-08-27 18:21:47 +0800

    fix:修改部署檔案

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
